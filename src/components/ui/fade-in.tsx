e0df7ffbf65ee0e87950ecfaeb45db7927f33bd7
PranamNK <pranamnk90@gmail.com>
2026-03-05 21:12:12 +0530

    fix: update GSAP import in fade-in component for Astro build

--- src/components/ui/fade-in.tsx
+++ src/components/ui/fade-in.tsx
@@ -2,7 +2,7 @@
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
